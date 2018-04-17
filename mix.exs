3dd2dc58bb1b056850c6437001a062d59a0d9aa0
Christopher Coté <Christopher Cot>
2018-04-17 15:01:20 -0500

    bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule IEQBackhaul.Mixfile do
 
   def project do
     [app: :ieq_gateway,
-     version: "0.1.9",
+     version: "0.2.0",
      elixir: "~> 1.5",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
